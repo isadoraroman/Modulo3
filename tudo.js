b05511f8fca07a0c780421f239cd17589e9214a8
isadoraroman <isadora_roman-silva@estudante.sc.senai.br>
2019-06-23 01:30:47 -0300

    criamos função para renderizar tudo

--- tudo.js
+++ tudo.js
@@ -1,3 +1,31 @@
 var listElement = document.querySelector("#app ul")
 var inputElement = document.querySelector("#app input")
 var buttonElement = document.querySelector("#app button")
+
+function renderTodos() {
+
+    listElement.innerHTML = "";
+
+    for (todo of todos) {
+
+        var todoElement = document.createElement("li");
+        var todoText = document.createTextNode(todo);
+        var linkElement = document.createElement("a");
+
+        linkElement.setAttribute("href", "#");
+
+        var pos = todos.indexOf(todo);
+
+        linkElement.setAttribute("onclick", "DeleteTodo(" + pos + ")")
+
+        var linkText = document.createTextNode("Excluir");
+
+        linkElement.appendChild(linkText);
+
+        todoElement.appendChild(todoText);
+        todoElement.appendChild(linkElement);
+        listElement.appendChild(todoElement);
+
+    }
+
+}
